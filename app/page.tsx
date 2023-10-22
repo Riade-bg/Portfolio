636094dddc0af8e38647646d3f8356ee134ab384
Riade Boughaba <64991030+Riade-bg@users.noreply.github.com>
2023-10-22 08:37:59 +0000

    Update page.tsx and package.json

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export const dynamic = 'force-dynamic'
 export default function Home() {
   return (
       <>
-        {/* <Navbar /> */}
+        <Navbar />
         <Header />
         <About />
         <Work />
